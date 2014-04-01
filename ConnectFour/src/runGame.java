a774606aaf7f88b7eec801a2d224e10665fdcc23
James Miller <jpm@mm.st>
2014-04-01 09:49:21 +0100

    More commenting, got rid of redundant playerSearch object k

--- ConnectFour/src/runGame.java
+++ ConnectFour/src/runGame.java
@@ -10,17 +10,17 @@ public class runGame {
 
 	public static void main(String args[]) throws Exception
 	{
-		Board b; playerSearch p,k;
+		Board b; playerSearch p;
 		b=new Board(7,6);
 		p=new playerSearch();
-		k=new playerSearch();
 		
 		//Creates a BufferedReader object so that the user can interact with the game.
 		BufferedReader move = new BufferedReader(new 
 				InputStreamReader(System.in));
 
 		//Welcome Message
-		System.out.println("Welcome to ConnectFour!");
+		System.out.println("Welcome to ConnectFour! Here is your game board:");
+		System.out.println(b);
 			
 		//While loop uses winnerIs() and validMoves() functions from Board.java to determine whether
 		//The game is done
@@ -30,15 +30,16 @@ public class runGame {
 				//Prompt player 1 for move and make move
 				System.out.println("Player 1, enter a column number from 0-6 inclusive to drop your counter:");
 				b.makeMove(Integer.parseInt(move.readLine()));
-			}	else 
+			}	else  {
 				//Make player two's move
 				b.makeMove(p.getMove(b));
+			}
 			//Print the board to the terminal
 			System.out.println(b);
 			
 		}
 		
-
+		//Tells the user how the game has finished.
 		if(b.winnerIs() == 1) {
 			System.out.println("Player 1 WINS!");
 		} else if(b.winnerIs()==-1) {
